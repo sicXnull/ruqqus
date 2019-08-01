925933f9418b42faec6d54b5ae39625c2d2a4886
captainmeta4 <captainmeta4@gmail.com>
2019-07-31 21:11:30 -0400

    Update user.py

--- ruqqus/classes/user.py
+++ ruqqus/classes/user.py
@@ -229,7 +229,7 @@ class User(Base):
         page=int(page)
 
         notifications = db.query("Comment"
-                                 ).filter(text(f"parent_author_id={self.id}, read=false"
+                                 ).filter(text(f"parent_author_id={self.id} AND read=false"
                                              )).order_by(text("comments.created_utc DESC")
                                                         ).offset(25*(page-1)).limit(25).all()
 
